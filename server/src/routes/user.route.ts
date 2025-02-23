02d807b4621d00b3aac97e2907182b8f3e93138f
ericksjp <ericksjp703@gmail.com>
2025-02-23 19:24:19 -0300

    feat(server): Add route to delete a user by ID
    - Implement `deleteUserById` method in the user controller
    - Add a DELETE route to delete a user by ID in the user router
    - Fix `getUserSingleById` method in user router

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import { addUser, getUsers, getUserSingleById } from '../controllers/user.controller';
+import { addUser, deleteUserById, getUsers, getUserById } from '../controllers/user.controller';
 
 const userRoutes = Router();
 
 userRoutes.get('/', getUsers)
 userRoutes.post('/', addUser)
-userRoutes.get('/:id', getUserSingleById)
+userRoutes.get('/:id', getUserById)
+userRoutes.delete('/:id', deleteUserById)
 
 export default userRoutes;
