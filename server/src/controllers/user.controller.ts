4c746172e12e2cae2f11079621af933328c0d07b
ericksjp <ericksjp703@gmail.com>
2025-02-23 21:51:52 -0300

    feat(server): Add route to update a user by ID
    - Added `updateUser` function in `user.controller.ts` to handle user
      updates.
    - Included password hashing using `bcryptjs` in `updateUser`.
    - Enhanced `getUsers` to select specific attributes.
    - Improved error handling and response messages across all functions.
    - Updated `user.route.ts` to include the new PATCH `/users/:id` route.

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
 import User from "../models/user.model";
+import { hashSync } from "bcryptjs";
 
 export async function getUsers(req: Request, res: Response) {
-  const resp = await User.findAll();
-  res.send(resp);
+  try {
+    const resp = await User.findAll({
+      attributes: ["id", "name", "email", "profilePicture", "dateOfBirth"],
+    });
+    res.status(200).json(resp);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 export async function getUserById(req: Request, res: Response) {
@@ -18,10 +26,10 @@ export async function getUserById(req: Request, res: Response) {
 
     const { dataValues } = user;
 
-    res.status(200).json({...dataValues, password: undefined});
+    res.status(200).json({ ...dataValues, password: undefined });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
 
@@ -31,18 +39,50 @@ export async function deleteUserById(req: Request, res: Response) {
 
     const result = await User.destroy({
       where: {
-        id: userId
-      }
-    })
+        id: userId,
+      },
+    });
 
     if (result === 0) {
-      res.status(404).json({message: "User not found"})
+      res.status(404).json({ message: "User not found" });
       return;
     }
 
-    res.status(204).json()
-  } catch(error) {
-    console.error(error)
-    res.status(500).json({message: "Internal server error"})
+    res.status(204).json();
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
+
+export async function updateUser(req: Request, res: Response) {
+  try {
+    const userId = req.params.id;
+    const user = await User.findByPk(userId);
+
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    const { email, name, dateOfBirth, profilePicture } = req.body;
+    let { password } = req.body;
+
+    if (password) {
+      password = hashSync(password, 10);
+    }
+
+    const { dataValues } = await user.update({
+      email,
+      name,
+      dateOfBirth,
+      profilePicture,
+      password,
+    });
+
+    res.status(200).json({ ...dataValues, password: undefined });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import { deleteUserById, getUsers, getUserById } from '../controllers/user.controller';
+import { deleteUserById, getUsers, getUserById, updateUser } from '../controllers/user.controller';
 
 const userRoutes = Router();
 
 userRoutes.get('/', getUsers)
 userRoutes.get('/:id', getUserById)
 userRoutes.delete('/:id', deleteUserById)
+userRoutes.patch('/:id', updateUser)
 
 export default userRoutes;
