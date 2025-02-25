c1a10a63d9829c9d1f4455d32fbdc2e293d240b0
ericksjp <ericksjp703@gmail.com>
2025-02-25 18:50:02 -0300

    refactor(server): Update models and controllers
    - Renamed fields in migrations to camelCase for consistency
    - Simplified class controller methods by using Sequelize model methods
    - Removed associations.ts and userClass.model.ts, moved associations to
      model definitions
    - Updated model definitions to use static methods for initialization and
      associations
    - Adjusted routes to use camelCase parameter names
    - Minor fixes and improvements in various files

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { User } from "../models/associations";
+import { User } from "../models/";
+import { hashSync } from "bcryptjs";
 
 export async function getUsers(req: Request, res: Response) {
   try {
--- server/src/models/class.model.ts
+++ server/src/models/class.model.ts
@@ -1,66 +1,84 @@
 import { DataTypes, Model } from "sequelize";
 
-import db from ".";
 import User from "./user.model";
+import { Sequelize } from "sequelize";
 
 type LocationType = {
-    type: "Point";
-    coordinates: [number, number];
-}
+  type: "Point";
+  coordinates: [number, number];
+};
 
 class Class extends Model {
-    public readonly id!: string;
-    public name!: string;
-    public subject!: string;
-    public institution!: string;
-    public status!: string;
-    public location?: LocationType;
-    public teacherId!: string;
-}
+  public readonly id!: string;
+  public name!: string;
+  public subject!: string;
+  public institution!: string;
+  public status!: string;
+  public location?: LocationType;
+  public teacherId!: string;
 
-Class.init(
-{
-    id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    subject: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    institution: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    status: {
-        type: DataTypes.ENUM,
-        values: ["Ativo", "Arquivado"],
-        allowNull: false
-    },
-    location: {
-        type: DataTypes.GEOMETRY('POINT'),
-        allowNull: true
-    },
-    teacherId: {
-        type: DataTypes.UUID,
-        allowNull: false,
-        references: {
-            model: User,
-            key: "id"
+   public static associate(models: { User: typeof User }) {
+    this.belongsToMany(models.User, {
+      through: 'user_class',
+      foreignKey: 'classId',
+      otherKey: 'userId',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+
+    this.belongsTo(models.User, {
+      foreignKey: 'teacherId',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
+  }
+
+  public static initialize(sequelize: Sequelize) {
+    this.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          defaultValue: DataTypes.UUIDV4,
+          primaryKey: true,
         },
-        field: "teacher_id"
-    }
-},
-{
-    tableName: "classes",
-    sequelize: db,
-    timestamps: false
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        subject: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        institution: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        status: {
+          type: DataTypes.ENUM,
+          values: ["Ativo", "Arquivado"],
+          allowNull: false,
+        },
+        location: {
+          type: DataTypes.GEOMETRY("POINT"),
+          allowNull: true,
+        },
+        teacherId: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          references: {
+            model: User,
+            key: "id",
+          },
+          field: "teacherId",
+        },
+      },
+      {
+        sequelize,
+        modelName: "Class",
+        tableName: "classes",
+        timestamps: false,
+      },
+    );
+  }
 }
-);
-
-export default Class;
+export default Class
