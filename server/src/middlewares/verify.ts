b557aa21ef39ba1346c23543eeb8aaa1ec9227f1
Gerlandio <gerlandios51@gmail.com>
2025-02-26 18:31:43 -0300

    fix: fixing undefined classId on userClass middleware

--- server/src/middlewares/verify.ts
+++ server/src/middlewares/verify.ts
@@ -5,7 +5,6 @@ export async function verifyUserClass(req:Request, res:Response, next:NextFuncti
     try {
         const {id} = req.body;
         const classId = req.params.classId;
-        console.log(classId)
         const _class = await Class.findOne({where:{id:classId, teacherId: id}})
         if(!_class){
             res.status(401).json({message:"User not part off class"});
