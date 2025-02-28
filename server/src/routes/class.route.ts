62909a846df6f0125c6be247c56352dadf4acd44
ericksjp <ericksjp703@gmail.com>
2025-02-28 15:58:20 -0300

    feat(server): Implement CRUD operations for students in a class
    - Implement `getStudents`, `getStudent`, `addStudent`, and
      `removeStudent` functions in the class controller to manage students
      in the database.
    - Add routes in the class router to expose the new controller functions
      via the API.
    - Update the class model with `addUser` and `removeUser` methods for
      TypeScript typing.

--- server/src/routes/class.route.ts
+++ server/src/routes/class.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { createClass, deleteClass, getClassById, getClasses, updateClass, updateClassStatus } from "../controllers/class.controller";
+import { addStudent, createClass, deleteClass, getClassById, getClasses, getStudent, getStudents, removeStudent, updateClass, updateClassStatus } from "../controllers/class.controller";
 import auth from "../middlewares/auth";
 import lessonRoutes from "./lesson.route";
 import feedbackRoutes from "./feedback.route";
+import { verifyUserClass } from "../middlewares/verify";
 
 const classRoutes = Router();
 
@@ -13,6 +14,16 @@ classRoutes.get("/:classId", auth, getClassById);
 classRoutes.put("/:classId", auth, updateClass);
 classRoutes.patch("/:classId", auth, updateClassStatus);
 classRoutes.delete("/:classId", auth, deleteClass);
+
+// Route for student exit the class
+classRoutes.delete("/:classId/drop", auth, removeStudent)
+
+// Routes for teacher actions related to students in a class
+classRoutes.get("/:classId/student", auth, verifyUserClass, getStudents)
+classRoutes.get("/:classId/student/:studentId", auth, verifyUserClass, getStudent)
+classRoutes.post("/:classId/student", auth, verifyUserClass, addStudent)
+classRoutes.delete("/:classId/student/:studentId", auth, verifyUserClass, removeStudent)
+
 classRoutes.use("/:classId/lesson",auth, lessonRoutes);
 classRoutes.use("/:classId/lesson/:lessonId/feedback", auth, feedbackRoutes);
 
