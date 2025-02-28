5387894ab9a808f7c08fa5f1190c797925191342
ericksjp <ericksjp703@gmail.com>
2025-02-28 20:04:42 -0300

    feat(server): Add Zod schemas for input validation
    - Add `zod` dependency to `package.json`.
    - Implement `class.schema.ts` for validating class input data.
    - Implement `feedback.schema.ts` for validating feedback input data.
    - Implement `lesson.schema.ts` for validating lesson input data.
    - Implement `user.schema.ts` for validating user input data.
    - Export all schemas in `index.ts` for easier imports.

---
+++ server/src/schemas/class.schema.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+const ClassInput = z.object({
+  name: z.string()
+    .trim()
+    .min(3, { message: "Name must be at least 3 characters long" })
+    .max(255, { message: "Name must be at most 100 characters long" }),
+  
+  subject: z.string()
+    .trim()
+    .min(3, { message: "Subject must be at least 3 characters long" })
+    .max(255, { message: "Subject must be at most 100 characters long" }),
+
+  institution: z.string()
+    .trim()
+    .min(3, { message: "Institution must be at least 3 characters long" })
+    .max(255, { message: "Institution must be at most 100 characters long" }),
+
+  status: z.enum(["Ativo", "Arquivado"], { message: "Invalid status" }),
+
+  location: z.tuple([z.number(), z.number()]).optional(),
+
+  teacherId: z.string()
+    .uuid({ message: "Invalid teacher ID" }),
+});
+
+export default ClassInput;
---
+++ server/src/schemas/user.schema.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+
+const UserInput = z.object({
+  name: z.string()
+    .trim()
+    .min(3, { message: "Name must be at least 3 characters long" })
+    .max(255, { message: "Name must be at most 255 characters long" }),
+
+  email: z.string()
+    .trim()
+    .email({ message: "Invalid email" })
+    .max(255, { message: "Name must be at most 255 characters long" }),
+
+  profilePicture: z.string()
+    .trim()
+    .url({ message: "Invalid URL" }),
+
+  dateOfBirth: z.date()
+    .refine(date => date <= new Date(), { message: "Date of birth must be in the past" }),
+
+  password: z.string()
+    .trim()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(255, { message: "Password must be at most 255 characters long"})
+});
+
+export default UserInput;
