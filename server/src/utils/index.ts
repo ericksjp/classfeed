fd495b07853c2718b0f2e3644a5df6866b87a0a1
ericksjp <ericksjp703@gmail.com>
2025-03-01 01:10:49 -0300

    feat(server): Add error handling wrappers for controllers
    - Added `tryCatchWrapper` function to wrap a single controller with
      try-catch block.
    - Added `multiTryCatchWrapper` function to wrap multiple controllers
      with try-catch blocks.

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,8 +1,31 @@
 import jwt from "jsonwebtoken";
 import authConfig from "../config/authConfig";
+import { NextFunction, Request, Response } from "express";
 
 export function generateToken(str: string) {
   return jwt.sign({ id: str }, authConfig.secret, {
     expiresIn: authConfig.expiresIn,
   });
+}
+
+export function tryCatchWrapper(controller: (req: Request, res: Response, next?: NextFunction) => Promise<unknown | void> | void) {
+  return async function (req: Request, res: Response, next: NextFunction) {
+    try {
+      await controller(req, res, next);
+    } catch (error) {
+      return next(error);
+    }
+  };
+}
+
+export function multiTryCatchWrapper(controllers: ((req: Request, res: Response, next: NextFunction) => Promise<unknown | void> | void)[]) {
+  return async function (req: Request, res: Response, next: NextFunction) {
+    for (const controller of controllers) {
+      try {
+        await controller(req, res, next);
+      } catch (error) {
+        return next(error);
+      }
+    }
+  };
 }
