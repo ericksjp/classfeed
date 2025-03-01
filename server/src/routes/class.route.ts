eecf73d0349cba06f930e1fb9e12245f750a6033
ericksjp <ericksjp703@gmail.com>
2025-02-28 23:43:52 -0300

    refactor(server): improve verify middleware
    - Refactor `verify.ts` to introduce separate middleware functions:
      - `isUserPartOfClass`: Checks if a user is part of a class.
      - `isUserTeacher`: Ensures the user is the teacher of the class.
      - `isUserStudent`: Ensures the user is a student in the class.
    - Update route handlers to use these specific middlewares for better
      authorization control.
    - Add `Users` association in the class model.
    - Remove redundant `package-lock.json`.

--- server/src/routes/class.route.ts
+++ server/src/routes/class.route.ts
@@ -4,27 +4,28 @@ import { addStudent, createClass, deleteClass, getClassById, getClasses, getStud
 import auth from "../middlewares/auth";
 import lessonRoutes from "./lesson.route";
 import feedbackRoutes from "./feedback.route";
-import { verifyUserClass } from "../middlewares/verify";
+import { isUserPartOfClass, isUserStudent, isUserTeacher } from "../middlewares/verify";
 
 const classRoutes = Router();
 
 classRoutes.post("/", auth, createClass);
 classRoutes.get("/", auth, getClasses);
-classRoutes.get("/:classId", auth, getClassById);
-classRoutes.put("/:classId", auth, updateClass);
-classRoutes.patch("/:classId", auth, updateClassStatus);
-classRoutes.delete("/:classId", auth, deleteClass);
+classRoutes.get("/:classId", auth, isUserPartOfClass, getClassById);
+classRoutes.put("/:classId", auth, isUserTeacher, updateClass);
+classRoutes.patch("/:classId", auth, isUserTeacher, updateClassStatus);
+classRoutes.delete("/:classId", auth, isUserTeacher, deleteClass);
 
 // Route for student exit the class
-classRoutes.delete("/:classId/drop", auth, removeStudent)
+classRoutes.delete("/:classId/drop", auth, isUserStudent, removeStudent)
 
 // Routes for teacher actions related to students in a class
-classRoutes.get("/:classId/student", auth, verifyUserClass, getStudents)
-classRoutes.get("/:classId/student/:studentId", auth, verifyUserClass, getStudent)
-classRoutes.post("/:classId/student", auth, verifyUserClass, addStudent)
-classRoutes.delete("/:classId/student/:studentId", auth, verifyUserClass, removeStudent)
+classRoutes.get("/:classId/student", auth, isUserTeacher, getStudents)
+classRoutes.get("/:classId/student/:studentId", auth, isUserTeacher, getStudent)
+classRoutes.post("/:classId/student", auth, isUserTeacher, addStudent)
+classRoutes.delete("/:classId/student/:studentId", auth, isUserTeacher, removeStudent)
 
 classRoutes.use("/:classId/lesson",auth, lessonRoutes);
+
 classRoutes.use("/:classId/lesson/:lessonId/feedback", auth, feedbackRoutes);
 
 export default classRoutes;
--- server/src/routes/lesson.route.ts
+++ server/src/routes/lesson.route.ts
@@ -1,14 +1,14 @@
 import {Router} from "express";
 import * as lessonController from "../controllers/lesson.controller";
-import {verifyUserClass} from "../middlewares/verify"
+import {isUserPartOfClass, isUserTeacher} from "../middlewares/verify"
 
 const lessonRoutes = Router({ mergeParams: true});
 
-lessonRoutes.get("/",verifyUserClass, lessonController.getlessons);
-lessonRoutes.get("/:lessonId", verifyUserClass, lessonController.getLessonById);
-lessonRoutes.post("/", lessonController.createLesson);
-lessonRoutes.patch("/:lessonId", verifyUserClass, lessonController.updateLesson);
-lessonRoutes.delete("/:lessonId",verifyUserClass, lessonController.deleteLesson);
+lessonRoutes.get("/",isUserPartOfClass, lessonController.getlessons);
+lessonRoutes.get("/:lessonId", isUserPartOfClass, lessonController.getLessonById);
+lessonRoutes.post("/", isUserTeacher, lessonController.createLesson);
+lessonRoutes.patch("/:lessonId", isUserTeacher, lessonController.updateLesson);
+lessonRoutes.delete("/:lessonId",isUserTeacher, lessonController.deleteLesson);
 
 
 export default lessonRoutes;
