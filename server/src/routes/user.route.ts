7c35b7eea78dd7fa050cd4d1557b4f47144a9b48
ericksjp <ericksjp703@gmail.com>
2025-03-01 10:37:39 -0300

    refactor(server): enhance error handling in user controller
    - Wrap user routes with `multiTryCatchWrapper` for consistent error
      handling
    - Integrate `UserInput` schema validation in user controller functions
      to ensure request data integrity
    - Use custom error classes in the user controller functions.
    - Refactor `multyTryCatchWrapper` to return an array of wrapped
      functions

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import UserController from "../controllers/user.controller";
 import Auth from "../middlewares/auth";
+import { multiTryCatchWrapper } from "../utils";
 
 const userRoutes = Router();
 
-userRoutes.get("/", Auth, UserController.get);
-userRoutes.delete("/", Auth, UserController.remove);
-userRoutes.patch("/", Auth, UserController.update);
+userRoutes.get("/", multiTryCatchWrapper([Auth, UserController.get]));
+userRoutes.delete("/", multiTryCatchWrapper([Auth, UserController.remove]));
+userRoutes.patch("/", multiTryCatchWrapper([Auth, UserController.update]));
 
 export default userRoutes;
