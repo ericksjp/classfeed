56c63eebe4c0d6d73187ae8cf9aa02b27ab037d0
ericksjp <ericksjp703@gmail.com>
2025-03-14 15:00:58 -0300

    feat(server): Add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import rootRouter from "./routes";
 import errorHandler from "./middlewares/errorHandler";
+import cors from "cors";
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors());
 
 app.use('/api', rootRouter);
 
