9aa8c22c24eeeea6bd21387e8a9a0a4bbea0c661
ericksjp <ericksjp703@gmail.com>
2025-03-14 00:10:33 -0300

    fix(server): Prevent server reload during image operations
    - Modifie docker-compose.example.yaml to include new volume for file storage
    - Add server/.env.example with example environment variables
    - Update server/src/app.ts to use FILE_STORAGE_PATH from environment variables
    - Remove uploads folder

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,13 @@
 import express from "express"
 import rootRouter from "./routes";
 import errorHandler from "./middlewares/errorHandler";
-import path from "path";
 
 const app = express();
 app.use(express.json())
 
 app.use('/api', rootRouter);
 
-app.use('/uploads', express.static(path.resolve("uploads")));
+app.use('/uploads', express.static(process.env.FILE_STORAGE_PATH as string));
 
 app.use(errorHandler);
 
